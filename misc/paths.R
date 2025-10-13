a45c8cc0c9dfc4277ff0c6d37df15e4d48d1954e
Clàudia Vicente <02claudiavicentecomorera@gmail.com>
2025-10-13 13:12:32 +0200

    update report

--- misc/paths.R
+++ misc/paths.R
@@ -1,3 +1,36 @@
 library(glue)
 library(here)
+library(dplyr)
+
+
+## Data
+dt <- "data"
+sn_dt <- glue("{dt}/snRNAseq")
+st_dt <- glue("{dt}/visium")
+
+
+## Outputs
+out <- "output"
+plots <- glue("{out}/plots")
+html <- glue("{out}/html")
+
+
+## Reports
+rep_plots <- glue("{plots}/reports/")
+rep_html <- glue("{html}/reports")
+
+
+## Read metadata
+load_sample_metadata <- function() {
+  sample_metadata <- glue::glue("{st_dt}/sample-metadata.csv") %>%
+    here::here() %>%
+    readr::read_csv() %>%
+    mutate(
+      tissue = if_else(tissue == "lymph_node", "lymph-node", tissue),
+      donor_tissue = glue::glue("{donor_id}-{tissue}"),
+      study_id_tissue = glue::glue("{study_id}-{tissue}")
+    )
+  
+  return(sample_metadata)
+}
 
